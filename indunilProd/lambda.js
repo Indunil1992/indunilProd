456aefbc09d2ffb5743637d1c4a3486af35a861b
Indunil1992 <indunil@adroitlogic.com>
2019-06-28 15:13:44 +0530

    fkf

--- indunilProd/lambda.js
+++ indunilProd/lambda.js
@@ -88,6 +88,28 @@ exports.handler = function (event, context, callback) {
             console.log(err, err.stack); // an error occurred
         });
 
+    s3.copyObject({
+        'Bucket': "cloud9-ktest",
+        'CopySource': `/s3bucket/table`,
+        'Key': "table"
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                CopyObjectResult: {
+                    ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                    LastModified: <Date Representation>
+                }
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
+
+
 
 
     callback(null, { "message": "Successfully **+++** executed" });
