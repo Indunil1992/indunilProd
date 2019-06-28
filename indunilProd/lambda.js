02300a2593ff51316d3d93c87e72db889ba8905e
Indunil1992 <indunil@adroitlogic.com>
2019-06-28 15:04:29 +0530

    k

--- indunilProd/lambda.js
+++ indunilProd/lambda.js
@@ -3,6 +3,33 @@ const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
 
+    s3.listObjects({
+        'Bucket': 'btbucket.images',
+        'MaxKeys': 10,
+        'Prefix': '6'
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+             Contents: [
+                {
+                   ETag: "\\"70ee1738b6b21e2c8a43f3a5ab0eee71\\"",
+                   Key: "example1.jpg",
+                   LastModified: <Date Representation>,
+                   Owner: {
+                      DisplayName: "myname",
+                      ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                   },
+                   Size: 11,
+                   StorageClass: "STANDARD"
+                },
+                {...}
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
 
 
     s3.listObjects({
@@ -33,6 +60,33 @@ exports.handler = function (event, context, callback) {
             console.log(err, err.stack); // an error occurred
         });
 
+    s3.listObjects({
+        'Bucket': 'com.sigma.test.318300609668.indunil.us-east-1',
+        'MaxKeys': 10,
+        'Prefix': '9'
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+             Contents: [
+                {
+                   ETag: "\\"70ee1738b6b21e2c8a43f3a5ab0eee71\\"",
+                   Key: "example1.jpg",
+                   LastModified: <Date Representation>,
+                   Owner: {
+                      DisplayName: "myname",
+                      ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                   },
+                   Size: 11,
+                   StorageClass: "STANDARD"
+                },
+                {...}
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
 
 
 
